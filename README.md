4d9af681ac5371a8c3215d16846ba97a5d52ad97
LuizClaudio <cazarin.junior@gmail.com>
2022-08-28 13:07:29 -0300

    update readme

--- README.md
+++ README.md
@@ -1,27 +1,10 @@
 # Projeto escola - Vuetify
-### Luiz Claudio
-
-## Navegue até a pasta do projeto
-cd project-school/
-
-## Instale as dependências
-```
-npm install
-```
 
 ### Inicie a aplicação
 ```
 npm run serve
 ```
-
-### Compilar e diminuir para produção
-```
-npm run build
-```
-
- ![acessar](https://github.com/luiz-cazarin/project-school/blob/main/readme-guia.jpg)
- 
- ### Use o seguinte endereço para acessar a pagina
- ``
- http://localhost:8080/
- ``
+### Use o seguinte endereço para acessar a pagina
+``
+http://localhost:8080/
+``
--- src/views/Students.vue
+++ src/views/Students.vue
@@ -1,80 +1,66 @@
 <template>
   <!-- estudantes -->
   <div class="students pa-6">
-    <v-container>     
+    <v-container>
       <v-row>
-      <v-list-item>
-        <v-col cols="12" md="4">
-          <v-text-field
-            v-model="newStudentName"
-            :counter="50"
-            label="Nome"
-            required
-            clearable
-          ></v-text-field>
-        </v-col>
-        <v-col cols="12" md="4">
-          <v-text-field
-            v-model="newStudentRegistration"
-            :counter="10"
-            label="Matricula"
-            required
-            clearable
-          ></v-text-field>
-        </v-col>
-        
+        <v-list-item>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="newStudentName"
+              :counter="50"
+              label="Nome"
+              required
+              clearable
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="newStudentRegistration"
+              :counter="10"
+              label="Matricula"
+              required
+              clearable
+            ></v-text-field>
+          </v-col>
         </v-list-item>
       </v-row>
 
-        <v-btn
-          text
-          class="ma-2"
-          elevation="2"
-          @click="addStudent"
-        >
+      <v-btn text class="ma-2" elevation="2" @click="addStudent">
         Cadastrar
-        </v-btn>
-
+      </v-btn>
     </v-container>
     <!-- Lista de Alunos -->
 
     <v-expansion-panels>
       <v-expansion-panel v-for="student in students" :key="student.id">
         <v-expansion-panel-header>
-        <v-list-item-title>
-            Nome: {{ student.name }}
-        </v-list-item-title>
+          <v-list-item-title> Nome: {{ student.name }} </v-list-item-title>
           <v-list-item-subtitle>
-              Matricula: {{ student.registration }}
+            Matricula: {{ student.registration }}
           </v-list-item-subtitle>
-              <v-btn
-              icon
-              max-width="40"
-              plain
-              @click="deleteStudent(student.id)"
-              > 
-                <v-icon color="red">mdi-delete</v-icon>
-              </v-btn>
+          <v-btn icon max-width="40" plain @click="deleteStudent(student.id)">
+            <v-icon color="red">mdi-delete</v-icon>
+          </v-btn>
         </v-expansion-panel-header>
 
-        <v-expansion-panel-content v-for="discipline in student.disciplines":key="discipline.id">
-          Materia: {{discipline.name}}
+        <v-expansion-panel-content
+          v-for="discipline in student.disciplines"
+          :key="discipline.id"
+        >
+          Materia: {{ discipline.name }}
           <v-divider></v-divider>
           Professor: {{ discipline.teacher }}
-           <v-divider></v-divider>
-          Nota: {{ discipline.grade}}
-           
+          <v-divider></v-divider>
+          Nota: {{ discipline.grade }}
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
-  
   </div>
 </template>
 
 
 <script>
-
-import disciplines from './Discipline'
+import disciplines from "./Discipline";
 
 export default {
   name: "Students",
@@ -88,38 +74,36 @@ export default {
           name: "Algoritmo",
           teacher: "Wesley",
           grade: 7,
-          done: false
+          done: false,
         },
         {
           id: 2,
           name: "Logica matematica",
           teacher: "Adalberto",
           grade: 10,
-          done: false
+          done: false,
         },
         {
           id: 3,
           name: "Machine learning",
           teacher: "Daniele",
           grade: 8.9,
-          done: false
+          done: false,
         },
         {
           id: 4,
           name: "Data Science",
           teacher: "Vilson",
           grade: 9.3,
-          done: false
+          done: false,
         },
-        ],
-      students: [
-      {},
       ],
+      students: [{}],
     };
   },
 
-  components:{
-    disciplines
+  components: {
+    disciplines,
   },
 
   methods: {
@@ -129,19 +113,17 @@ export default {
     informationStudent(id) {
       this.students = this.students.filter(student.id === id);
     },
-    addStudent(){
+    addStudent() {
       let newStudent = {
         id: Date.now(),
         name: this.newStudentName,
         registration: this.newStudentRegistration,
-          disciplines: this.disciplines,
-      }
+        disciplines: this.disciplines,
+      };
       this.students.push(newStudent);
-      this.newStudentName = '',
-      this.newStudentRegistration= ''
+      (this.newStudentName = ""), (this.newStudentRegistration = "");
       console.log(this.students);
     },
-
   },
 };
 </script>
